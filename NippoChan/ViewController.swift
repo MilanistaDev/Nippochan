451536a8ea42a0d255cd6ffcccca52bce9b8f596
Takuya Aso <milanista224@gmail.com>
2018-12-12 20:13:24 +0900

    Initial Commit

---
+++ NippoChan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NippoChan
+//
+//  Created by 麻生 拓弥 on 2018/12/12.
+//  Copyright © 2018年 com.ASTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
